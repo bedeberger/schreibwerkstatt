37be535b0f51a3c540d6dac2307a29fa33d3b52e
David Berger <david.berger@dotag.ch>
2026-06-27 15:11:57 +0200

    recherche-board improved

--- public/sw-manifest.js
+++ public/sw-manifest.js
@@ -4,7 +4,7 @@
 //
 // __SHELL_BUILD: content hash over all listed assets → drives SHELL_CACHE.
 // __SHELL_MANIFEST: the coherent set precached atomically at SW install.
-self.__SHELL_BUILD = "2c12e6287f6c3298";
+self.__SHELL_BUILD = "ff3156015e32c795";
 self.__SHELL_MANIFEST = [
   "/css/admin/admin-home.css",
   "/css/admin/admin-settings.css",
