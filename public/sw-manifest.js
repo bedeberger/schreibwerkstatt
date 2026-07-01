144474f7bf09d5f81450419bfb259afcc0659ab0
David Berger <bede.berger@gmail.com>
2026-07-01 12:47:43 +0000

    erzählprofil aufgehübschter

--- public/sw-manifest.js
+++ public/sw-manifest.js
@@ -4,7 +4,7 @@
 //
 // __SHELL_BUILD: content hash over all listed assets → drives SHELL_CACHE.
 // __SHELL_MANIFEST: the coherent set precached atomically at SW install.
-self.__SHELL_BUILD = "01acf909c2bca3a0";
+self.__SHELL_BUILD = "da42d367a26247f6";
 self.__SHELL_MANIFEST = [
   "/css/admin/admin-home.css",
   "/css/admin/admin-settings.css",
