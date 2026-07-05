91798ff76af1bb90c8b8623797e4915ea69cbb3a
David Berger <david.berger@dotag.ch>
2026-07-05 20:54:01 +0200

    mobile responseness verbessert

--- public/sw-manifest.js
+++ public/sw-manifest.js
@@ -4,7 +4,7 @@
 //
 // __SHELL_BUILD: content hash over all listed assets → drives SHELL_CACHE.
 // __SHELL_MANIFEST: the coherent set precached atomically at SW install.
-self.__SHELL_BUILD = "95621a215e0ed75f";
+self.__SHELL_BUILD = "355398a3f8e12297";
 self.__SHELL_MANIFEST = [
   "/css/admin/admin-home.css",
   "/css/admin/admin-settings.css",
