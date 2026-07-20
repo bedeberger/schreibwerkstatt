f5633d5f3d8a3b360744625cc8cc99918c74caef
David Berger <david.berger@dotag.ch>
2026-07-20 18:22:04 +0200

    pagetree mobile

--- public/sw-manifest.js
+++ public/sw-manifest.js
@@ -4,7 +4,7 @@
 //
 // __SHELL_BUILD: content hash over all listed assets → drives SHELL_CACHE.
 // __SHELL_MANIFEST: the coherent set precached atomically at SW install.
-self.__SHELL_BUILD = "2bfe6d9715484b78";
+self.__SHELL_BUILD = "d3f86b3b8c9aa50e";
 self.__SHELL_MANIFEST = [
   "/css/admin/admin-backup.css",
   "/css/admin/admin-home.css",
