99ac28d8cd975a0ad2542f02555a3d0e203a8fd4
David Berger <david.berger@dotag.ch>
2026-07-20 08:39:50 +0200

    übersetzung nachgezogen

--- public/sw-manifest.js
+++ public/sw-manifest.js
@@ -4,7 +4,7 @@
 //
 // __SHELL_BUILD: content hash over all listed assets → drives SHELL_CACHE.
 // __SHELL_MANIFEST: the coherent set precached atomically at SW install.
-self.__SHELL_BUILD = "773bb7238b3dcc7d";
+self.__SHELL_BUILD = "7ffc33cf5583770a";
 self.__SHELL_MANIFEST = [
   "/css/admin/admin-backup.css",
   "/css/admin/admin-home.css",
